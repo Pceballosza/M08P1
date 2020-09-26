e9d5530b6fefb2ebc3c762c98d1ab1d8fe08daf7
Pablo <Pablo@PC>
2020-09-26 16:58:59 +0200

    pcz - Funcion nº1

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
             function PCZ_Funcion1 (){
-                echo "Texto del ejemplo"
+                echo "Texto del ejemplo";
             }
 
         ?>
